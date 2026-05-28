bebffaf6be45a319a114d6839f1edb4b434aa712
Gustavo Medori <mail@gustavo.sh>
2026-05-27 22:54:40 -0600

    Update fish greeting with elephino joke

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,3 +1,5 @@
 function fish_greeting
-	fortune | cowsay | lolcat
+	echo "What do you get when you cross an elephant with a rhino?
+
+	¯\\_(ツ)_/¯ Hell if I know" | cowsay -f stegosaurus | lolcat
 end
